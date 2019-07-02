502714bbb3a8869cc89d1bf9d06b5238eb92d794
Maxime Quoilin <max.quoilin@gmail.com>
2019-07-02 14:32:57 +0200

    commit pre test event

--- src/event/activity/activity.component.ts
+++ src/event/activity/activity.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Activity } from '../models/activity.model';
 import { ActivityService } from '../services/activity.service';
+import { publishDefaultGlobalUtils } from '@angular/core/src/render3/global_utils';
 
 @Component({
   selector: 'app-activity',
@@ -14,6 +15,12 @@ export class ActivityComponent implements OnInit {
   constructor(private activityService: ActivityService) { }
 
   ngOnInit() {
+    /* or (const activity of this.activities) {
+      let date = new Date(activity.dateDebut);
+      activity.dateDebut = date.toLocaleDateString('fr-FR');
+      console.log(activity.dateDebut);
+      console.log(date);
+    } */
     this.activityService.getActivities().subscribe(activity => this.activities = activity);
   }
 }
---
+++ src/pipes/hour.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'hour'
+})
+export class HourPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return null;
+  }
+
+}
