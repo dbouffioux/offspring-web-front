1fa30472d1b33a50fb86771b2002c7c029bc84ab
Maxime Quoilin <max.quoilin@gmail.com>
2019-07-03 08:37:59 +0200

    commit pre resolution bug lien

--- src/event/events-routing.module.ts
+++ src/event/events-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
     {
       path: '/activity/:id',
       component: ActivityItemComponent,
+    },
+    {
+      path: '/activity/',
+      component: ActivityItemComponent,
     }
 ];
 
