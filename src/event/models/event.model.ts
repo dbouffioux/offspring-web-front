b6f0a2b9530736a70e9ed3866e35d1a48dfc95bf
Damien Bouffioux <damien.bouffioux@gmail.com>
2019-07-09 09:38:21 +0200

    added activity properties

--- src/event/models/event.model.ts
+++ src/event/models/event.model.ts
@@ -8,5 +8,7 @@ export class Event {
   public heureDebut: string;
   public heureFin: string;
   public creatorId: string;
+  public start: any;
+  public end: any;
   public listActivity?: Activity[];
 }
